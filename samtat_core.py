0ac82ef0507a040a62c1cf4dc3c46298457b87bd
Hamed Ebrahimi <H.Ebrahimi@mmr.local>
2022-09-20 17:28:56 +0430

    start signal slot2

--- samtat_core.py
+++ samtat_core.py
@@ -1,9 +1,3 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QVBoxLayout
-from PyQt5.QtWidgets import QWidget
 from PyQt5 import QtCore, QtGui, QtWidgets
 from samtat_ui import Ui_SamtatReport
 
