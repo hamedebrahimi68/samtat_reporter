807fb102d41c1b0dc332f1d124b2c594b0c789e0
Hamed Ebrahimi <H.Ebrahimi@mmr.local>
2022-09-20 17:22:21 +0430

    start signal slot

--- samtat_core.py
+++ samtat_core.py
@@ -7,27 +7,13 @@ from PyQt5.QtWidgets import QWidget
 from PyQt5 import QtCore, QtGui, QtWidgets
 from samtat_ui import Ui_SamtatReport
 
-# Filename: signals_slots.py
-"""Signals and slots."""
 
-def show_msg():
-    """Slot function."""
-    if msg.text():
-        msg.setText("")
-    else:
-        msg.setText("اتصال به دیتا بیس لطفا صبر کنید...")
 
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle('گزارش تردد سامانه سمتات')
-layout = QVBoxLayout()
-
-btn = QPushButton('گزارش گیری')
-btn.clicked.connect(show_msg)  # Connect clicked to show_msg()
-
-layout.addWidget(btn)
-msg = QLabel('')
-layout.addWidget(msg)
-window.setLayout(layout)
-window.show()
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    SamtatReport = QtWidgets.QMainWindow()
+    ui = Ui_SamtatReport()
+    ui.setupUi(SamtatReport)
+    SamtatReport.show()
+    sys.exit(app.exec_())
