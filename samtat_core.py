f7e58611da4b6523331cfcca25a2d5da3fbc981a
hamed ebrhimi <h.ebrahimi6895@gmail.com>
2022-09-21 11:12:30 +0430

    mongo db query added

--- samtat_core.py
+++ samtat_core.py
@@ -1,7 +1,7 @@
-import imp
 from PyQt5 import QtCore, QtGui, QtWidgets
 from samtat_ui import Ui_SamtatReport
 import sys
+
 
 class ui_events(QtWidgets.QMainWindow):
     def __init__(self):
@@ -9,6 +9,7 @@ class ui_events(QtWidgets.QMainWindow):
         self.ui = Ui_SamtatReport()
         self.ui.setupUi(self)
         self.ui.search_taradod.clicked.connect(self.search_taradod)
+        self.ui.image_downloder.clicked.connect(self.search_tasvir)
 
     def search_taradod(self):
         try:
@@ -23,14 +24,21 @@ class ui_events(QtWidgets.QMainWindow):
             pelak_combo = self.ui.pelak_combo.currentIndex()
             takhalof = self.ui.takhalof_binary.checkState()
             self.oracle_query(self,start_time,end_time,ostan,control_station,class_name,takhalof,pelak_p1,pelak_p2,pelak_p3,pelak_combo)
-            self.table
         except:
             pass
+
+    def search_tasvir(self):
+        try:
+            taradod_id = self.ui.textline_taradod.text()
+            self.mongodb_query(self,taradod_id)
+        except:
+            pass
+
 
 def oracle_query(self,start_time,end_time,ostan,control_station,class_name,takhalof,pelak_p1,pelak_p2,pelak_p3,pelak_combo):
     try:
         import cx_Oracle
-        connection = cx_Oracle.connect(user="hr", password=userpwd, dsn="dbhost.example.com/orclpdb1")
+        connection = cx_Oracle.connect(user="root", password= 'userpwd', dsn="dbhost.example.com/orclpdb1")
         # Obtain a cursor
         cursor = connection.cursor()
         # Execute the query
@@ -39,12 +47,27 @@ def oracle_query(self,start_time,end_time,ostan,control_station,class_name,takha
          WHERE start_time BETWEEN :start AND :end """
 
         cursor.execute(sql, start=start_time, end=end_time)
-        # Loop over the result
+        # Loop over the result and set result tabel
         for row in cursor:
-            self.table
-           tableWidget->setItem(row, column, newItem)
+            self.ui.table_output.setItem(row, column, newItem)
+        
     except:
-        pass
+        self.ui.
+def mongodb_query(self,taradod_id):
+    import pymongo
+
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["taradod_database"]
+
+    mycol = mydb["tasvir_table"]
+
+    myquery = { "taradod_id": taradod_id }
+
+    mydoc = mycol.find(myquery)
+
+    for x in mydoc:
+        print(x)
+
 
 
 if __name__ == "__main__":
